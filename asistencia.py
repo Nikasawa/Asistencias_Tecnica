2bb080c5e056ac2532b5ef6370a4053eaf47e696
Nikasawa <121456466+Nikasawa@users.noreply.github.com>
2024-03-15 19:51:07 -0300

    Objetivos principales

---
+++ asistencia.py
@@ -0,0 +1,17 @@
+# 3.1415926535897937
+
+# Conectar con MySQL
+# Supervisar hora de ingreso de alumnos. Aplicar faltas o llegadas tarde dependiendo la hora de llegada
+# Justificativos. Ej: Medico, accidente y/o ilegalidad/es
+# Retiros y/o actualizacion de horarios
+# Reconocer al alumno por su huella digital
+# Sumar cuando falta (Decir si se excedio en las faltas)
+
+# Sistema GUI
+    # Probar con pantalla (Tele o compu conexion HDMI)
+
+# Hacer mini DB para ir viendo:
+    # Asistencias
+    # Inasistencias
+    # Huellas digitales
+
